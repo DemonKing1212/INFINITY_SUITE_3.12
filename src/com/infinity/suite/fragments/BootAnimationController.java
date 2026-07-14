aac297cb709e3571f14c235a53666dc5be78af42
Shivam <shivamwwe48@gmail.com>
2026-07-14 12:35:51 +0000

    bug fixes

--- src/com/infinity/suite/fragments/BootAnimationController.java
+++ src/com/infinity/suite/fragments/BootAnimationController.java
@@ -3,14 +3,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package org.evolution.settings.fragments.themes;
+package com.infinity.suite.fragments;
 
 import android.content.Context;
 
-import com.android.internal.util.evolution.PixelPropsUtils;
 import com.android.settings.core.BasePreferenceController;
 
-import org.evolution.settings.utils.BootAnimationUtils;
+import com.infinity.suite.utils.BootAnimationUtils;
 
 public class BootAnimationController extends BasePreferenceController {
 
@@ -20,11 +19,6 @@ public class BootAnimationController extends BasePreferenceController {
 
     @Override
     public int getAvailabilityStatus() {
-        // Custom fork builds spoof Pixel props, so check this first
-        // to avoid incorrectly hiding the preference on non-Pixel fork devices.
-        if (PixelPropsUtils.isCustomForkBuild()) {
-            return UNSUPPORTED_ON_DEVICE;
-        }
         if (BootAnimationUtils.isBootAnimationSelectorDisabled()) {
             return UNSUPPORTED_ON_DEVICE;
         }
